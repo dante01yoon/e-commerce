a39578dcaa8a9f5211e3f40af37f6b5fff319803
risel <bunggl@naver.com>
2020-05-07 04:29:11 +0900

    adding pinterest layout

--- frontend/src/components/card/index.tsx
+++ frontend/src/components/card/index.tsx
@@ -2,17 +2,23 @@ import React, { FC } from 'react';
 import * as Styled from './style';
 import { SHOP_DATA } from 'src/models/data'; 
 export const CardContainer:FC = () => {
+  let items = new Array(); 
   let slicedData = SHOP_DATA[0].items.slice();
-  const items = []; 
   while(slicedData.length > 0){
+    let cardItems = new Array(); 
     for( const [index, value] of slicedData.splice(0,3).entries()){
-      items.push(<Styled.CardItem key={index}/>)
+      cardItems.push(<Styled.CardItem key={index}/>)
     }
+    items = [...items, 
+      <Styled.SemiContainer key={Math.floor(~~1*Math.random() * 0xff)}>
+        {cardItems}
+      </Styled.SemiContainer>
+    ]
   }
   return(
     <section>
       <Styled.Container>
-        
+        {items}
       </Styled.Container>
     </section>
   )
--- frontend/src/components/card/style.tsx
+++ frontend/src/components/card/style.tsx
@@ -5,22 +5,60 @@ export const CardSection = styled.section`
 
 export const CardItem = styled.div`
   width: 272px;
-  height: 384px; 
   display:flex;
   align-items: center; 
   background-color: aqua; 
 `;
-
+export const SemiContainer = styled.div`
+  flex-flow: column wrap;
+`
 export const Container = styled.div`
   overflow: auto; 
   margin: 0 auto;
   width: 1128px;
   display:flex;
   justify-content: center;
-  flex-flow: column wrap;
-  ${CardItem}:nth-child(2n+3) {
-    height: 248px;
-    background-color: orange;
+  flex-flow: row wrap;
+  ${SemiContainer}:nth-child(1){
+    ${CardItem}:nth-child(1) {
+      height: 248px;
+      background-color: orange;
+    }
+    ${CardItem}:nth-child(2) {
+      height: 328px;
+      background-color: green;
+    }
+    ${CardItem}:nth-child(3) {
+      height: 196px;
+      background-color: purple;
+    }
   }
-  
+  ${SemiContainer}:nth-child(2){
+    ${CardItem}:nth-child(1) {
+      height: 196px;
+      background-color: gray;
+    }
+    ${CardItem}:nth-child(2) {
+      height: 248px;
+      background-color: orange;
+    }
+    ${CardItem}:nth-child(3) {
+      height: 328px;
+      background-color: purple;
+    }
+  }
+  ${SemiContainer}:nth-child(3){
+    ${CardItem}:nth-child(1) {
+      height: 328px;
+      background-color: blue;
+    }
+    ${CardItem}:nth-child(2) {
+      height: 328px;
+      background-color: black;
+    }
+    ${CardItem}:nth-child(3) {
+      height: 384px;
+      background-color: green;
+    }
+  }
 `

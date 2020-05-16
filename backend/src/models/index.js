bcd6ebe4bd0bb7b4381806ae83dd4782c0b95ed0
risel <bunggl@naver.com>
2020-05-17 05:45:46 +0900

    veryifySignUp build

---
+++ backend/src/models/index.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise; 
+
+const db = {};
+db.mongoose = mongoose;
+db.user = require("./user.model");
+db.role = require("./role.model");
+
+db.ROLES = ["users", "admin", "moderator"]; 
+
+
+
+module.exports = {
+  db
+
+};
